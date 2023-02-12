3f41999b3a635c01209e2ba7fbef1bdd2362d825
Jack-LP <jackpaget1@gmail.com>
2023-02-12 17:43:40 +0000

    Added main sections

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        charcoal: '#131313',
+        bone: '#f1f1f2',
+        sky: '#027aef',
+      },
+    },
   },
   plugins: [],
 };
